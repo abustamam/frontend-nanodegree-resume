d1b53def2f5dfbc43d088afd3b448d99bd896516
Rasheed Bustamam <rasheed.bustamam@gmail.com>
2015-02-20 22:49:41 -0800

    Fix spacing issue

--- js/helper.js
+++ js/helper.js
@@ -50,10 +50,10 @@ var HTMLschoolLocation = "<div class='location-text'>%data%</div>";
 var HTMLschoolMajor = "<em><br>Major: %data%</em>"
 
 var HTMLonlineClasses = "<h3>Online Classes</h3>";
-var HTMLonlineURL = "<br><a href='%data%'>";
+var HTMLonlineURL = "<a href='%data%'>";
 var HTMLonlineTitle = "%data%";
 var HTMLonlineSchool = " - %data%</a>";
-var HTMLonlineDates = "<div class='date-text'>%data%</div>";
+var HTMLonlineDates = "<div class='date-text'>%data%</div><br>";
 
 var internationalizeButton = "<button>Internationalize</button>";
 var googleMap = "<div id='map'></div>";
